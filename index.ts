dab4ce59b7f797b3ac0d396532d01260daa0aeac
Hiroki Horiuchi <horiuchi.g@gmail.com>
2023-08-25 11:40:45 +0900

    feat: update TypeScript

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ const packageJson: {
     name: string;
     version: string;
     description: string;
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
 } = require('./package.json');
 
 const singleQuote: Plugin = {
@@ -19,26 +19,27 @@ const singleQuote: Plugin = {
 
 // eslint-disable-next-line @typescript-eslint/require-await
 async function postProcess(
-    _pluginContext: PluginContext
+    _pluginContext: PluginContext,
 ): Promise<ts.TransformerFactory<ts.SourceFile> | undefined> {
-    return (context: ts.TransformationContext) => (
-        root: ts.SourceFile
-    ): ts.SourceFile => {
-        function visit(node: ts.Node): ts.Node {
-            node = ts.visitEachChild(node, visit, context);
+    return (context: ts.TransformationContext) =>
+        (root: ts.SourceFile): ts.SourceFile => {
+            function visit<T extends ts.Node>(node: T): T {
+                node = ts.visitEachChild(node, visit, context);
 
-            if (ts.isStringLiteral(node)) {
-                // `singleQuote` is internal property.
-                // via: https://github.com/microsoft/TypeScript/blob/v3.9.2/src/compiler/types.ts#L1353
-                ((node as unknown) as {
-                    singleQuote: boolean;
-                }).singleQuote = true;
+                if (ts.isStringLiteral(node)) {
+                    // `singleQuote` is internal property.
+                    // via: https://github.com/microsoft/TypeScript/blob/v3.9.2/src/compiler/types.ts#L1353
+                    (
+                        node as unknown as {
+                            singleQuote: boolean;
+                        }
+                    ).singleQuote = true;
+                }
+
+                return node;
             }
-
-            return node;
-        }
-        return ts.visitNode(root, visit);
-    };
+            return ts.visitNode(root, visit, ts.isSourceFile);
+        };
 }
 
 export default singleQuote;
